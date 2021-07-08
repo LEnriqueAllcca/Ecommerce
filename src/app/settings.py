4b446585d5491e1917fef9558eedcf30ba878eba
Enrique <luisenriqueallcca@gmail.com>
2021-07-08 02:15:11 -0500

    v1

--- src/app/settings.py
+++ src/app/settings.py
@@ -139,6 +139,89 @@ if DEBUG is False:
     SECURE_SSL_REDIRECT = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+    AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+    ]
+
+    SITE_ID = 1
+
+    AUTHENTICATION_BACKENDS = [
+        'django.contrib.auth.backends.ModelBackend',
+        'allauth.account.auth_backends.AuthenticationBackend',
+    ]
+
+    INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    'crispy_forms',
+    'widget_tweaks',
+
+    'core',
+    'cart',
+    ]
+
+    DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+    NOTIFY_EMAIL = env('NOTIFY_EMAIL')
+
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
+
+    ROOT_URLCONF = 'app.urls'
+
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [os.path.join(BASE_DIR, 'templates')],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
+
+    WSGI_APPLICATION = 'app.wsgi.application'
+
+
+    ACCOUNT_AUTHENTICATION_METHOD = 'email'
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED =False
+    ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+    CRISPY_TEMPLATE_PACK= 'bootstrap4'
+    LOGIN_REDIRECT_URL = '/'
+
     ALLOWED_HOSTS = ['https://jardines_paisajes_bosques.com']
 
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -148,6 +231,12 @@ if DEBUG is False:
     EMAIL_HOST_USER=env('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD=env('EMAIL_HOST_PASSWORD')
 
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+    STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
     DATABASES = {
         'default':{
             'ENGINE': 'django.db.backends.sqlite3',
@@ -155,7 +244,6 @@ if DEBUG is False:
         }
 
     }
-
 
     PAYPAL_CLIENT_ID=env('PAYPAL_LIVE_CLIENT_ID')
     PAYPAL_SECRET_KEY=env('PAYPAL_LIVE_SECRET_KEY')
